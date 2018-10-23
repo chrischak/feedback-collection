73002a3340db6f2d98cd2831e0ac2d5827863742
cchak <cchak@example.com>
2018-10-23 15:49:27 -0400

    save google profile into user collection

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("user");
 
 passport.use(
   new GoogleStrategy(
@@ -13,6 +16,7 @@ passport.use(
       console.log("access token", accessToken);
       console.log("refresh token", refreshToken);
       console.log("profile", profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
