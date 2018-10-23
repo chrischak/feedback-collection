ad169791bc8a4a7376de9e9dfba84dd3d96d46b3
cchak <cchak@example.com>
2018-10-23 15:59:33 -0400

    fixed typo in passport user->users

--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,8 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
-const User = mongoose.model("user");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
